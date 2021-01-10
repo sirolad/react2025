9e21006d0fe1a34e29a95e9735d7752d1f77766b
Surajudeen Akande <sirolad@gmail.com>
2021-01-10 23:59:05 +0100

    Social Auth with Firebase

---
+++ utils/auth.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useContext, createContext } from 'react'
+import firebase from './firebase'
+import Router from 'next/router'
+
+const authContext = createContext()
+export function AuthProvider({ children }) {
+    const auth = useProvideAuth()
+    return <authContext.Provider value={auth}>{children}</authContext.Provider>
+}
+export const useAuth = () => {
+    return useContext(authContext)
+}
+function useProvideAuth() {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const handleUser = (rawUser) => {
+        if (rawUser) {
+            const user = formatUser(rawUser)
+            setLoading(false)
+            setUser(user)
+            return user
+        } else {
+            setLoading(false)
+            setUser(false)
+            return false
+        }
+    }
+    const signinWithGitHub = () => {
+        setLoading(true)
+        return firebase
+            .auth()
+            .signInWithPopup(new firebase.auth.GithubAuthProvider())
+            .then((response) => handleUser(response.user))
+    }
+    const signout = () => {
+        return firebase
+            .auth()
+            .signOut()
+            .then(() => handleUser(false))
+    }
+    const signinWithGoogle = (redirect) => {
+        setLoading(true)
+        return firebase
+            .auth()
+            .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+            .then((response) => {
+                handleUser(response.user)
+                if (redirect) {
+                    Router.push(redirect)
+                }
+            })
+    }
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged(handleUser)
+        return () => unsubscribe()
+    }, [])
+    return {
+        user,
+        loading,
+        signinWithGitHub,
+        signout,
+        signinWithGoogle
+    }
+}
+const formatUser = (user) => {
+    return {
+        uid: user.uid,
+        email: user.email,
+        name: user.displayName,
+        provider: user.providerData[0].providerId,
+        photoUrl: user.photoURL,
+    }
+}
+
